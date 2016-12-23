6de9822ee8c4750f6a84cab9e128d0cdf18ddc4e
vushnu <matveykarp@yandex.com>
2016-12-23 15:49:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var app = express()
 
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname))
 
 app.get('/', function(req, res) {
   res.sendFile('index.html')
 })
 
-app.listen('8080')
+app.listen(app.get('port'))
